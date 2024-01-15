06e02511463fcfe980288e26c366f2fe1764e5b3
unknown <ismail.ouali@ump.ac.ma>
2024-01-15 16:19:28 +0100

    fixing errors

--- index.php
+++ index.php
@@ -42,13 +42,13 @@ $objwiki = wiki::showLastWikis();
                         <a href="index.php" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Acceuil</a>
                     </li>
                     <li>
-                        <a href="Wiki-panel.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Gerer vos Wikis</a>
-                    </li>
-                    <li>
                         <?php
                             if(@$_SESSION['id_user']){
 
                                 ?>
+                        <a href="Wiki-panel.php" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Gerer vos Wikis</a>
+                    </li>
+                    <li>
                         <a href="controller/log-out.php" class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Se Deconnecter</a>
                         <?php
                             }else{
