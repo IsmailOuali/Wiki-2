809f3fb1184fcf57031b9ca18f828fdab8435c69
unknown <ismail.ouali@ump.ac.ma>
2024-01-16 10:21:28 +0100

    adding index path

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ $objwiki = wiki::showLastWikis();
     
     <nav class="bg-gradient-to-r from-gray-700 via-gray-900 to-black border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <a href="index.php" class="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Wiki</span>
             </a>
